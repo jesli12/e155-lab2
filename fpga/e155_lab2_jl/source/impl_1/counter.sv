c75c6c0c8f5239c90fc26abd8dac754fb8e7f079
Jessica <jesli@g.hmc.edu>
2026-09-12 04:24:36 -0700

    First Stab at ALL MODULES
    top, counter, sev seg, scanner
    instatiated 2 counters, one in top for mux action on sev seg anode powering, one in the submodule scanner to do the 2 hz cycling to keypad

---
+++ fpga/e155_lab2_jl/source/impl_1/counter.sv
@@ -0,0 +1,32 @@
+// Jessica Li  |  jesli@g.hmc.edu
+// 09/12/2026 (just name changes from lab1, no major changes)
+// This is a submodule
+// It contains a simple clock divider that takes the HSOSC input
+
+module counter
+	#(parameter WIDTH = 25,
+		MAX_COUNT = 10_000_000) (
+	input   logic   osc, nrst, en,
+	output  logic   clk,
+	output  logic   [WIDTH-1:0] count
+);
+
+	logic clk_state = 0;
+	logic [WIDTH-1:0] count = 0; // check this!
+	
+	always_ff @(posedge osc) begin
+			if (~nrst) begin
+				count <= 0;
+				clk_state <= 0;
+				end
+			else if (en) begin
+				if (count >= (MAX_COUNT-1)) begin //for exact timing MAX_COUNT - 1 to account for the cycle it takes to register that it hit max
+					clk_state <= ~clk_state; // toggles clk state on or off once desired cycle time up
+					count <= 0;
+				end
+			end
+	end
+		
+	assign clk = clk_state; 
+
+endmodule
---
+++ fpga/e155_lab2_jl/source/impl_1/lab2top_jl.sv
@@ -0,0 +1,67 @@
+// Jessica Li  |  jesli@g.hmc.edu
+// 09/12/2026
+// This is the top-level module, taking input from 4 switches and output on 3 LEDs and a 7 segment display.
+// It contains internal clock initialization (high-speed oscillator) and the switch-to-LED logic.
+// The top-level module also uses another module for switch-to-7-segment display, and another module for the counter.
+
+
+module lab1_jl(
+	input   logic  [3:0] sw1,
+	input   logic  [3:0] sw2,
+	input   logic  [3:0] col
+	input   logic nreset,
+	input   logic enable,  // do i make these signals internal?
+	output  logic  [1:0] pwr,
+	output  logic  [6:0] seg,
+	output  logic  [3:0] row,
+	output  logic  [3:0] led  //
+);
+
+	logic int_osc;
+	logic seg_clk;
+	logic [3:0] sw; // this is the single set of switches that get sent into the single seven segment module
+	
+	// Internal high-speed oscillator, 48 MHz clock generated in FPGA by HSOSC primitive
+	HSOSC hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
+	
+	// counter for timing multiplexer (120 Hz)
+	counter (
+		.WIDTH(20),
+		.MAX_COUNT(200_000) // MAX_COUNT = 200_000 = a signal on/off frequency of 120 Hz
+	) segment_counter (
+		.osc (int_osc), 
+		.nrst (nreset),  
+		.en (enable),
+		.clk (seg_clk),
+		.count () // purposely ignored, no use
+	);
+	
+	// switch-to-7 segment display module
+	sev_seg segment_decoder(
+		.switch (sw), 
+		.segment (seg) // this already outputs for segment display
+	);
+	
+	assign pwr = (seg_clk == 1'b0)? 2'b10 : 2'b01; // MUX: seg_clk == 0 --> pwr = 10 powers first display, seg_clk ==1 --> pwr = 01 powers second display
+	assign sw = (seg_clk == 1'b0)? sw1 : sw2;	   // MUX: seg_clk == 0 --> sw1 + first display on, seg_clk ==1 --> sw2 + second display on (see above)
+	
+	// #### KEYPAD!! ####
+	
+	// connect submodule SCAN: input = osc_count reset enable, output = row[3:0] to keypad | outputs: 1000 (row[0]), 0100 (row[1]), 0010 (row[2]), and 0001 (row[3])
+	
+	scanner scanning(
+		.int_osc (int_osc), 
+		.nreset (nreset), 
+		.enable (enable),
+		.rows (row)
+	);
+	
+	
+	// Note: col output is 0 when button pressed & row powered, pulled up to 1 when not pressed due to transistors
+	assign led[0] = (col == 4'b0111)? 1'b1 : 1'b0;
+	assign led[1] = (col == 4'b1011)? 1'b1 : 1'b0;
+	assign led[2] = (col == 4'b1101)? 1'b1 : 1'b0;
+	assign led[3] = (col == 4'b1110)? 1'b1 : 1'b0;
+	
+
+endmodule
