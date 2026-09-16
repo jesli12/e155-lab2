72540926329a24c6bb1b9d80b2cd441582bf3b62
Jessica <jesli@g.hmc.edu>
2026-09-15 18:10:57 -0700

    top test bench in progress

--- fpga/e155_lab2_jl/source/impl_1/lab2top_jl_tb.sv
+++ fpga/e155_lab2_jl/source/impl_1/lab2top_jl_tb.sv
@@ -2,8 +2,8 @@
 // 09/15/2026
 // This is a test bench the top-level module of lab 2.
 /* The following tests include:
-	1. Exercise multiplexing functionality
-	2. LED driving functionality.
+	1. LED driving functionality (column input to LEDs).
+	2. Exercise multiplexing functionality (7-segment switches and displays)
 */
 
 `timescale 1 ns/1 ns
@@ -34,17 +34,58 @@ module lab2top_jl_tb();
 	initial begin
 		nrst = 0; //nrst is active low
 		en = 1;
-		#20
+		#20;
 		nrst = 1;
-		// ### 1. Does the Scanner submodule exert all 4 output transitions? ###############################################################################
-		// 20 ns per posedge if osc (counter increment) 20 ns * 6_000_000 = time per cycle = 120_000_000
+		sw1s = 4'b1111;
+		sw2s = 4'b0000;
 		
-		$display("### Test 1. Does the Scanner submodule exert all 4 output transitions? ");
-		#500; // avoiding edge by 500 ns
-		assert (row == 4'b1000)
-			$display ("1a. Success Output Row 0: row = 1000. Time: %0t.", $time);
-		else $error("1a. Failure Output Row 0: row does not equal 1000, but it should be the first state. Time: %0t.", $time);
-		#120_000_000
+		// ### 1. LED driving functionality (column input to LEDs)###############################################################################
+		$display("### Test 1. LED Driving functionality from column input. ");
+		cols = 4'b0111;
+		#20;
+		assert (leds == ~cols)
+			$display ("1a. Success. Time: %0t.", $time);
+		else $error("1a. Failure. Time: %0t.", $time);
+		cols = 4'b1011;
+		#20;
+		assert (leds == ~cols)
+			$display ("1b. Success. Time: %0t.", $time);
+		else $error("1b. Failure. Time: %0t.", $time);
+		cols = 4'b1101;
+		#20;
+		assert (leds == ~cols)
+			$display ("1c. Success. Time: %0t.", $time);
+		else $error("1c. Failure. Time: %0t.", $time);
+		cols = 4'b1110;
+		#20;
+		assert (leds == ~cols)
+			$display ("1d. Success. Time: %0t.", $time);
+		else $error("1d. Failure. Time: %0t.", $time);
+		cols = 4'b0011;
+		#20;
+		assert (leds == ~cols)
+			$display ("1e. Success. Time: %0t.", $time);
+		else $error("1e. Failure. Time: %0t.", $time);
+		cols = 4'b0101;
+		#20;
+		assert (leds == ~cols)
+			$display ("1f. Success. Time: %0t.", $time);
+		else $error("1f. Failure. Time: %0t.", $time);
+		cols = 4'b1111;
+		#20;
+		
+		// ### 2. Exercise multiplexing functionality (7-segment switches and displays)#########################################################
+		$display("### Test 2. Exercise multiplexing functionality (7-segment switches and displays)");
+		#18_000_000;
+		sw1s = 4'b1000;
+		sw2s = 4'b0001;
+		#450_000_000;
+		#110_000_000;
+		nrst = 0;
+		#18_000_000;
+		nrst = 1;
+		en = 0;
+		#110_000_000;
 		
 		$stop;
 	end
--- fpga/e155_lab2_jl/source/impl_1/scanner_tb.sv
+++ fpga/e155_lab2_jl/source/impl_1/scanner_tb.sv
@@ -33,7 +33,7 @@ module scanner_tb();
 	initial begin
 		nrst = 0; //nrst is active low
 		en = 1;
-		#20
+		#20;
 		nrst = 1;
 		// ### 1. Does the Scanner submodule exert all 4 output transitions? ###############################################################################
 		// 20 ns per posedge if osc (counter increment) 20 ns * 6_000_000 = time per cycle = 120_000_000
@@ -43,15 +43,15 @@ module scanner_tb();
 		assert (row == 4'b1000)
 			$display ("1a. Success Output Row 0: row = 1000. Time: %0t.", $time);
 		else $error("1a. Failure Output Row 0: row does not equal 1000, but it should be the first state. Time: %0t.", $time);
-		#120_000_000
+		#120_000_000;
 		assert (row == 4'b0100)
 			$display ("1b. Success Output Row 1: row = 0100. Time: %0t.", $time);
 		else $error("1b. Failure Output Row 1: row does not equal 0100, but it should be the second state. Time: %0t.", $time);
-		#120_000_000
+		#120_000_000;
 		assert (row == 4'b0010)
 			$display ("1c. Success Output Row 2: row = 0010. Time: %0t.", $time);
 		else $error("1c. Failure Output Row 2: row does not equal 0010, but it should be the third state. Time: %0t.", $time);
-		#120_000_000
+		#120_000_000;
 		assert (row == 4'b0001)
 			$display ("1d. Success Output Row 3: row = 0001. Time: %0t.", $time);
 		else $error("1d. Failure Output Row 3: row does not equal 0001, but it should be the fourth state. Time: %0t.", $time);
@@ -62,24 +62,24 @@ module scanner_tb();
 			2c. nreset = 0 can set count = 0, which means scanner exerts row 0, thus led 0 turns on and stays on until nreset = 1
 		*/
 		en = 0; 
-		#120_000_000
-		#120_000_000
+		#120_000_000;
+		#120_000_000;
 		assert (row == 4'b0001)
 			$display ("2a. Success Enable Hold Row 3: row = 0001. Time: %0t.", $time);
 		else $error("2a. Failure Enable Hold Row 3: row does not equal 0001, but it should be the fourth state. Time: %0t.", $time);
 		en = 1;
-		#120_000_000
-		#120_000_000
+		#120_000_000;
+		#120_000_000;
 		assert (row == 4'b0100)
 			$display ("2b. Success Enable = 0: row state changes. Time: %0t.", $time);
 		else $error("2b. Failure Enable = 0: row state does not change as expected. Time: %0t.", $time);
-		#120_000_000
+		#120_000_000;
 		nrst = 0;
-		#120_000_000
+		#120_000_000;
 		assert (row == 4'b1000)
 			$display ("2c. Success nreset = 0: row state resets to state 1. Time: %0t.", $time);
 		else $error("2c. Failure nreset = 0: row state does not reset to state 1. Time: %0t.", $time);
-		
+		nrst = 1;
 		
 		#120_000_000;
 		$stop;
